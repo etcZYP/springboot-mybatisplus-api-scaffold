e3b7485900462030f00ae4a09b1abcb8b5dc15c2
etczyp <etczyp@qq.com>
2022-05-20 16:34:25 +0800

    init

---
+++ src/test/java/CodeGenerator.java
@@ -0,0 +1,71 @@
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.generator.FastAutoGenerator;
+import com.baomidou.mybatisplus.generator.config.OutputFile;
+import com.baomidou.mybatisplus.generator.config.TemplateConfig;
+import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
+import java.util.*;
+import java.util.function.Consumer;
+
+/**
+ * 代码生成器，根据数据表名称生成对应的Entity、Mapper、Service、Controller简化开发。
+ */
+public class CodeGenerator {
+    public static void main(String[] args) {
+        List<String> tables = new ArrayList<>();
+        tables.add("user");
+
+        FastAutoGenerator.create("jdbc:mysql://localhost:3306/test","root","root")
+                .globalConfig(builder -> {
+                    builder.author("zyp")               //作者
+                            .outputDir(System.getProperty("user.dir")+"\\src\\main\\java")    //输出路径(写到java目录)
+                            .enableSwagger()           //开启swagger
+                            .commentDate("yyyy-MM-dd")
+                            .fileOverride();            //开启覆盖之前生成的文件
+
+                })
+                .packageConfig(builder -> {
+                    builder.parent("com.company")
+                            .moduleName("project")
+                            .entity("entity")
+                            .service("service")
+                            .serviceImpl("service.impl")
+                            .controller("controller")
+                            .mapper("mapper")
+                            .xml("mapper")
+                            .pathInfo(Collections.singletonMap(OutputFile.xml,System.getProperty("user.dir")+"\\src\\main\\resources\\mapper"));
+                })
+                .strategyConfig(builder -> {
+                    builder.addInclude(tables)
+                            .addTablePrefix("")
+                            .serviceBuilder()
+                            .formatServiceFileName("%sService")
+                            .formatServiceImplFileName("%sServiceImpl")
+                            .entityBuilder()
+                            .enableLombok()
+                            .logicDeleteColumnName("deleted")
+                            .enableTableFieldAnnotation()
+                            .controllerBuilder()
+                            // 映射路径使用连字符格式，而不是驼峰
+                            .enableHyphenStyle()
+                            .formatFileName("%sController")
+                            .enableRestStyle()
+                            .mapperBuilder()
+                            //生成通用的resultMap
+                            .enableBaseResultMap()
+                            .superClass(BaseMapper.class)
+                            .formatMapperFileName("%sMapper")
+                            .enableMapperAnnotation()
+                            .formatXmlFileName("%sMapper");
+                })
+                .templateConfig(new Consumer<TemplateConfig.Builder>() {
+                    @Override
+                    public void accept(TemplateConfig.Builder builder) {
+                        // 实体类使用我们自定义模板
+                        builder.entity("template/myentity.java");
+                    }
+                })
+                .templateEngine(new FreemarkerTemplateEngine()) // 使用Freemarker引擎模板，默认的是Velocity引擎模板
+                .execute();
+    }
+
+}
